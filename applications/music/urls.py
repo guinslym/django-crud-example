86d33c81e92a151c2b80e28f3d80e1746eeed8c6
Guinslym <guinslym@gmail.com>
2017-02-28 18:01:04 -0500

    ulrs

--- applications/music/urls.py
+++ applications/music/urls.py
@@ -1,1 +1,14 @@
-urls.py
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+
+from .views import HomeView, AlbumListView
+
+urlpatterns = [
+    url(r'*', HomeView.as_view(), name='music_home'),
+   url(r'^list/$', AlbumListView.as_view(), name='music_list'),
+	#url(r'^lineup_post/(?P<pk>\d+)/$', lineupView.LineupDetailView.as_view(), name="post"),
+]
