aff72183ce90f910b94acc5544db7bf47a0c4b8c
Guinsly Mondesir <guinslym@gmail.com>
2017-03-01 11:18:54 -0500

    adding everything excep migrations

--- applications/cardealer/urls.py
+++ applications/cardealer/urls.py
@@ -1,1 +1,32 @@
-urls.py
+from django.conf.urls.static import static
+from django.conf.urls import include, url
+
+from .views import (
+	#Other
+	HomeView,
+	CarListView,
+	#CRUD
+	CarCreateView,
+	CarDetailView,
+	CarUpdateView,
+	CarDeleteView
+	)
+
+
+urlpatterns = [
+   url(r'^home/$', HomeView.as_view(), name='cardealer_home'),
+   url(r'^list/$', CarListView.as_view(), name='car_list'),
+
+
+   #########
+   # CRUD  #
+   #########
+   #Create
+   url(r'^create/$', CarCreateView.as_view(), name='car_create'),
+   #Read (Detail/Show)
+   url(r'^(?P<pk>\d+)/$', CarDetailView.as_view(), name='car_detail'),
+   #Update
+   url(r'^update/(?P<pk>\d+)/$', CarUpdateView.as_view(), name='car_update'),
+   #Delete
+   url(r'^delete/(?P<pk>\d+)/$', CarDeleteView.as_view(), name='car_delete'),
+]
