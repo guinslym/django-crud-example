5544ff6c27f44f14fc8519d0fdbd572d13c57dc1
Guinsly Mondesir <guinslym@gmail.com>
2017-03-01 09:24:12 -0500

    model

--- applications/music/models.py
+++ applications/music/models.py
@@ -10,13 +10,10 @@ from datetime import datetime, timedelta
 def upload_to(instance, filename):
     return "%s/%s" % (instance._meta.app_label, filename)
 
-class Album(TimeStampedModel, models.Model):
+class Article(TimeStampedModel, models.Model):
     #user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=30, blank=False, null=False )
-    artist_name = models.CharField(max_length=30, blank=False, null=False )
-    nb_title = models.PositiveSmallIntegerField()
     description = models.TextField(max_length=500, blank=True, null=True )
-    birth_date = models.DateField(default="1995-01-01")
     image = models.ImageField(
                     upload_to=upload_to,
                     null=True, blank=True
@@ -26,9 +23,9 @@ class Album(TimeStampedModel, models.Model):
     	return str(self.title)
 
     class Meta:
-        verbose_name_plural = "Albums"
-        verbose_name = "Album"
+        verbose_name_plural = "Articles"
+        verbose_name = "Article"
 
 
     def get_absolute_url(self):
-        return reverse('album_detail', kwargs={'pk':self.id})
+        return reverse('article_detail', kwargs={'pk':self.id})
